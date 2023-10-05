3ece66ddbf84a470857556dcdf0dd83fc5cda261
Bonaventure Ogeto <bonaogeto1@gmail.com>
2023-10-05 11:35:23 +0300

    working with axios

---
+++ usersAxios.js
@@ -0,0 +1,10 @@
+const axios = require("axios");
+
+axios
+  .get("https://jsonplaceholder.typicode.com/users")
+  .then(function (response) {
+    console.log(response.data);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
