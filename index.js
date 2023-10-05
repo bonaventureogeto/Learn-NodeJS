00bf905ed22ca264b8873b3b224a3392decf2eeb
Bonaventure Ogeto <bonaogeto1@gmail.com>
2023-10-05 12:20:44 +0300

    logger module export

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 #!/usr/bin/env node
 
-const note = process.argv[2];
-const newNote = {
-  id: Date.now(),
-  note,
-}
-console.log('your new note', newNote)
+// const note = process.argv[2];
+// const newNote = {
+//   id: Date.now(),
+//   note,
+// }
+// console.log('your new note', newNote)
+
+const logger = require('./logger.js')
+
+logger.log("heyy")
+logger.log2(29)
---
+++ logger.js
@@ -0,0 +1,10 @@
+const logger = {
+    log: function (message) {
+        console.log(message);
+    },
+    log2: function (age) {
+        console.log("My age is", age)
+    }
+};
+
+module.exports = logger;
