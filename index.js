d39c395edd1a1c411fba14ed06bcccb867b72007
Bonaventure Ogeto <bonaogeto1@gmail.com>
2023-10-09 09:49:25 +0300

    updates

--- index.js
+++ index.js
@@ -6,16 +6,16 @@ export { greet } from './greet.js'
 export { fraud } from './fraud.js'
 
 
-// const note = process.argv[2];
-// const newNote = {
-//   id: Date.now(),
-//   note,
-// }
-// console.log('your new note', newNote)
+const note = process.argv[2];
+const newNote = {
+  id: Date.now(),
+  note,
+}
+console.log('your new note', newNote)
 
 // const logger = require('./logger.js');
 
 // logger.log("heyy");
 // logger.log2(29);
 
-console.log(add(3457, 65879));
+// console.log(add(3457, 65879));
