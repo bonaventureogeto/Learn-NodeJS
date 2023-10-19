f47ba567873e42932b10c213f47d43031f8df228
Bonaventure Ogeto <bonaogeto1@gmail.com>
2023-10-19 10:25:49 +0300

    importing and using routes

--- node-mongodb/app.js
+++ node-mongodb/app.js
@@ -16,7 +16,13 @@ mongoose
   .catch((err) => console.error("Error connecting to MongoDB:", err));
 
 // Define your routes here
+// importing routes
+const todosRouter = require("./routes/todos");
 
+// use routes
+app.use("/todos", todosRouter);
+
+// Start the server
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
