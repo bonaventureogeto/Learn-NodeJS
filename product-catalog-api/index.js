98e432c95f3918a1de39f07f772eadcbbdf5b396
Bonaventure Ogeto <bonaogeto1@gmail.com>
2023-10-19 09:58:18 +0300

    updates

--- product-catalog-api/index.js
+++ product-catalog-api/index.js
@@ -1,6 +1,27 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const { MongoClient } = require("mongodb");
+
+const username = encodeURIComponent("<username>");
+const password = encodeURIComponent("<password>");
+const cluster = "<clusterName>";
+const authSource = "<authSource>";
+const authMechanism = "<authMechanism>";
+let uri = `mongodb+srv://${username}:${password}@${cluster}/?authSource=${authSource}&authMechanism=${authMechanism}`;
+const client = new MongoClient(uri);
+async function run() {
+  try {
+    await client.connect();
+    const database = client.db("<dbName>");
+    const ratings = database.collection("<collName>");
+    const cursor = ratings.find();
+    await cursor.forEach((doc) => console.dir(doc));
+  } finally {
+    await client.close();
+  }
+}
+run().catch(console.dir);
 
 const app = express();
 const port = process.env.PORT || 3000;
