7c07fd9e9c7c4149dd51a8e19c7a2e3f977475a3
Bonaventure Ogeto <bonaogeto1@gmail.com>
2023-10-30 10:25:15 +0300

    authentication in nodejs

---
+++ authentication/config/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  DB_HOST:
+    "mongodb+srv://bonaogeto:admin@cluster0.9ogiu4s.mongodb.net/?retryWrites=true&w=majority",
+  TOKEN_SECRET: "DOUVB0Nxtp4uYRzgBlJ3TYfvbxe_TUDRNa48Zkw61Iw",
+};
---
+++ authentication/controllers/user.js
@@ -0,0 +1,135 @@
+const User = require("../models/user");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+const config = require("../config/config");
+const mongoose = require("mongoose");
+
+// Connect to MongoDB
+
+const db = config.DB_HOST;
+
+mongoose.connect(db)
+    .then(() => console.log("MongoDB connected..."))
+    .catch((err) => console.log(err));
+
+exports.register = async (req, res) => {
+  //Hash password
+  const salt = await bcrypt.genSalt(10);
+  const hasPassword = await bcrypt.hash(req.body.password, salt);
+
+  // Create an user object
+  let user = new User({
+    email: req.body.email,
+    name: req.body.name,
+    password: hasPassword,
+    user_type_id: req.body.user_type_id,
+  });
+
+  // Save User in the database
+    user
+      .save()
+      .then((registeredUser) => {
+        // create payload then Generate an access token
+        let payload = {
+          id: registeredUser._id,
+          user_type_id: req.body.user_type_id || 0,
+        };
+        const token = jwt.sign(payload, config.TOKEN_SECRET);
+        res.status(200).send({ token });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+};
+
+exports.login = async (req, res) => {
+    User.findOne({ email: req.body.email })
+        .then(async (user) => {
+            if (!user) {
+                return res.status(401).send("Mobile/Email or Password is wrong");
+            }
+            const validPass = await bcrypt.compare(
+                req.body.password,
+                user.password
+            );
+            if (!validPass)
+                return res.status(401).send("Mobile/Email or Password is wrong");
+
+            // Create and assign token
+            let payload = { id: user._id, user_type_id: user.user_type_id };
+            const token = jwt.sign(payload, config.TOKEN_SECRET);
+
+            res.status(200).header("auth-token", token).send({ token: token });
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+};
+
+// Access auth users only
+exports.userEvent = (req, res) => {
+    let events = [
+        {
+            "_id": "1",
+            "name": "Auto Expo",
+            "description": "lorem ipsum",
+            "date": "2012-04-23T18:25:43.511Z"
+        },
+        {
+            "_id": "2",
+            "name": "Auto Expo",
+            "description": "lorem ipsum",
+            "date": "2012-04-23T18:25:43.511Z"
+        },
+        {
+            "_id": "3",
+            "name": "Auto Expo",
+            "description": "lorem ipsum",
+            "date": "2012-04-23T18:25:43.511Z"
+        }
+    ]
+    res.json(events)
+};
+
+exports.adminEvent = (req, res) => {
+    let specialEvents = [
+        {
+            "_id": "1",
+            "name": "Auto Expo Special",
+            "description": "lorem ipsum",
+            "date": "2012-04-23T18:25:43.511Z"
+        },
+        {
+            "_id": "2",
+            "name": "Auto Expo Special",
+            "description": "lorem ipsum",
+            "date": "2012-04-23T18:25:43.511Z"
+        },
+        {
+            "_id": "3",
+            "name": "Auto Expo Special",
+            "description": "lorem ipsum",
+            "date": "2012-04-23T18:25:43.511Z"
+        },
+        {
+            "_id": "4",
+            "name": "Auto Expo Special",
+            "description": "lorem ipsum",
+            "date": "2012-04-23T18:25:43.511Z"
+        },
+        {
+            "_id": "5",
+            "name": "Auto Expo Special",
+            "description": "lorem ipsum",
+            "date": "2012-04-23T18:25:43.511Z"
+        },
+        {
+            "_id": "6",
+            "name": "Auto Expo Special",
+            "description": "lorem ipsum",
+            "date": "2012-04-23T18:25:43.511Z"
+        }
+    ]
+    res.json(specialEvents)
+
+};
